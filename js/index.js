9e2fe337e0b6069371a5110d785cf331a7cb96c8
Caio Magalhães <caiolopesfv@gmail.com>
2023-07-15 13:37:07 -0300

    finalizando html e css

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+const addTarefaForm = document.querySelector('.addTarefaForm'); 
+
+
+
+
+
+
+
+
+addTarefaForm.addEventListener('submit', (e) => {
+
+    e.preventDefault(); 
+
+    alert('botão cliado'); 
+
+})
